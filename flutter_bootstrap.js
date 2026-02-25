ec05cd146fc3da6f7fbf4626757b886cad035a0e
MohammedNafea <101453719+MohammedNafea@users.noreply.github.com>
2026-02-25 17:31:55 +0000

    Deploying to gh-pages from @ MohammedNafea/Chemistry-Initiative_hash-plus@5571410af790a460cf770f76c05c8644a82ce514 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2661326065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1127785757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
